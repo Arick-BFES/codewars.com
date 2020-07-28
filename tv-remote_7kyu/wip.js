cf9754340106b381d0c57517295dfb9c0e3bd008
Arick Armendariz <spider08@gmail.com>
2020-07-28 01:18:53 -0700

    Working on tv-remote_7kyu

---
+++ tv-remote_7kyu/wip.js
@@ -0,0 +1,22 @@
+var tvRemote = function(word) {
+    let r1 = [['a','b','c','d','e','1','2','3'],
+              ['f','g','h','i','j','4','5','6'],
+              ['k','l','m','n','o','7','8','9'],
+              ['p','q','r','s','t','.','@','0'],
+              ['u','v','w','x','y','z','_','/']]
+
+    let rHits = word.split('');
+    console.log(rHits)
+    console.log(r1.indexOf('b', 0))
+
+
+    return 1;
+  }
+  
+    console.log(tvRemote("does"), 16);
+    // console.log(tvRemote("your"), 23);
+    // console.log(tvRemote("solution"), 33);
+    // console.log(tvRemote("work"), 20);
+    // console.log(tvRemote("for"), 12);
+    // console.log(tvRemote("these"), 27);
+    // console.log(tvRemote("words"), 25);
